46f554c28627e282d8fa075e022eaf78c8fb9aba
Mateusz Szklarek <mateusz.szklarek@proton.ch>
2026-02-05 15:36:32 +0100

    Add assets, spacings, radiuses and color to LumoDesignSystem

---
+++ Modules/LumoDesignSystem/Sources/Icon.swift
@@ -0,0 +1,11 @@
+import DeveloperToolsSupport
+
+extension DS {
+    public enum Icon {
+        public static let arrowRight = ImageResource.icArrowRight
+        public static let globe = ImageResource.icGlobe
+        public static let microphone = ImageResource.icMicrophone
+        public static let paperClip = ImageResource.icPaperClip
+        public static let stop = ImageResource.icStop
+    }
+}
--- Modules/LumoDesignSystem/Sources/Icons.swift
+++
@@ -1,6 +0,0 @@
-import DeveloperToolsSupport
-
-public enum Icons {
-    public static let microphone = ImageResource.icMicrophone
-    public static let stop = ImageResource.icStop
-}
