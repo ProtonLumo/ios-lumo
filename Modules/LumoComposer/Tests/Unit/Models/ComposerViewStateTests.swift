29ce534e1a62566fa3878060b0ec20d28495cd19
Mateusz Szklarek <mateusz.szklarek@proton.ch>
2026-02-06 21:33:40 +0100

    Refactor ComposerViewStateTests to use parameterized tests

--- Modules/LumoComposer/Tests/Unit/Models/ComposerViewStateTests.swift
+++ Modules/LumoComposer/Tests/Unit/Models/ComposerViewStateTests.swift
@@ -27,142 +27,32 @@ struct ComposerViewStateTests {
 
     // MARK: - Action Button
 
-    @Test
-    func actionButton_IdleEmptyTextNotProcessing_ReturnsNone() {
-        let sut = ComposerViewState.testData(
-            currentText: "",
-            isProcessing: false,
-            mode: .idle
-        )
-
-        #expect(sut.actionButton == .none)
+    struct TestCase {
+        let given: ComposerViewState
+        let expected: ComposerView.ActionButtonState
     }
 
-    @Test
-    func actionButton_IdleHasTextNotProcessing_ReturnsSend() {
-        let sut = ComposerViewState.testData(
-            currentText: "What is AI?",
-            isProcessing: false,
-            mode: .idle
-        )
+    @Test(
+        arguments: [
+            TestCase(given: .testData(currentText: "", isProcessing: false, mode: .idle), expected: .none),
+            TestCase(given: .testData(currentText: "   \n  ", isProcessing: false, mode: .idle), expected: .none),
+            TestCase(given: .testData(currentText: "\n\n   \n  \n", isProcessing: false, mode: .idle), expected: .none),
+            TestCase(given: .testData(currentText: " ", isProcessing: false, mode: .idle), expected: .none),
+            TestCase(given: .testData(currentText: "\t", isProcessing: false, mode: .idle), expected: .none),
 
-        #expect(sut.actionButton == .send)
-    }
+            TestCase(given: .testData(currentText: "What is AI?", isProcessing: false, mode: .idle), expected: .send),
+            TestCase(given: .testData(currentText: "  Hello  \n", isProcessing: false, mode: .idle), expected: .send),
+            TestCase(given: .testData(currentText: "Line 1\nLine 2\nLine 3", isProcessing: false, mode: .idle), expected: .send),
 
-    @Test
-    func actionButton_IdleHasTextWithWhitespaceNotProcessing_ReturnsSend() {
-        let sut = ComposerViewState.testData(
-            currentText: "  Hello  \n",
-            isProcessing: false,
-            mode: .idle
-        )
-
-        #expect(sut.actionButton == .send)
-    }
-
-    @Test
-    func actionButton_IdleOnlyWhitespacesNotProcessing_ReturnsNone() {
-        let sut = ComposerViewState.testData(
-            currentText: "   \n  ",
-            isProcessing: false,
-            mode: .idle
-        )
-
-        #expect(sut.actionButton == .none)
-    }
-
-    @Test
-    func actionButton_ProcessingRegardlessOfModeAndText_ReturnsStop() {
-        // isProcessing=true, mode=.idle, empty text
-        let case1 = ComposerViewState.testData(
-            currentText: "",
-            isProcessing: true,
-            mode: .idle
-        )
-        #expect(case1.actionButton == .stop)
-
-        let case2 = ComposerViewState.testData(
-            currentText: "Test",
-            isProcessing: true,
-            mode: .idle
-        )
-        #expect(case2.actionButton == .stop)
-    }
-
-    @Test
-    func actionButton_WorkingModeRegardlessOfTextAndIsProcessing_ReturnsStop() {
-        // mode=.working, isProcessing=false, empty text
-        let case1 = ComposerViewState.testData(
-            currentText: "",
-            isProcessing: false,
-            mode: .working
-        )
-        #expect(case1.actionButton == .stop)
-
-        // mode=.working, isProcessing=false, has text
-        let case2 = ComposerViewState.testData(
-            currentText: "Test",
-            isProcessing: false,
-            mode: .working
-        )
-        #expect(case2.actionButton == .stop)
-    }
-
-    @Test
-    func actionButton_WhenBothProcessingAndWorking_ReturnsStop() {
-        let sut = ComposerViewState.testData(
-            currentText: "",
-            isProcessing: true,
-            mode: .working
-        )
-
-        #expect(sut.actionButton == .stop)
-    }
-
-    // MARK: - Edge Cases
-
-    @Test
-    func actionButton_MultilineText_ReturnsSend() {
-        let sut = ComposerViewState.testData(
-            currentText: "Line 1\nLine 2\nLine 3",
-            isProcessing: false,
-            mode: .idle
-        )
-
-        #expect(sut.actionButton == .send)
-    }
-
-    @Test
-    func actionButtonMultilineWhitespace() {
-        let sut = ComposerViewState.testData(
-            currentText: "\n\n   \n  \n",
-            isProcessing: false,
-            mode: .idle
-        )
-
-        #expect(sut.actionButton == .none)
-    }
-
-    @Test
-    func actionButtonSingleSpace() {
-        let viewState = ComposerViewState.testData(
-            currentText: " ",
-            isProcessing: false,
-            mode: .idle
-        )
-
-        #expect(viewState.actionButton == .none)
-    }
-
-    @Test
-    func actionButtonTabCharacter() {
-        let sut = ComposerViewState.testData(
-            currentText: "\t",
-            isProcessing: false,
-            mode: .idle
-        )
-
-        #expect(sut.actionButton == .none)
+            TestCase(given: .testData(currentText: "", isProcessing: true, mode: .idle), expected: .stop),
+            TestCase(given: .testData(currentText: "Test", isProcessing: true, mode: .idle), expected: .stop),
+            TestCase(given: .testData(currentText: "", isProcessing: false, mode: .working), expected: .stop),
+            TestCase(given: .testData(currentText: "Test", isProcessing: false, mode: .working), expected: .stop),
+            TestCase(given: .testData(currentText: "", isProcessing: true, mode: .working), expected: .stop),
+        ]
+    )
+    func actionButton(testCase: TestCase) {
+        #expect(testCase.given.actionButton == testCase.expected)
     }
 }
 
