afdb0db488a852eec06dac548f899418ab9968c5
Mateusz Szklarek <mateusz.szklarek@proton.ch>
2026-03-11 20:21:07 +0100

    Bump CFBundleVersion from 31 to 32

--- Project.swift
+++ Project.swift
@@ -3,7 +3,7 @@ import ProjectDescription
 // MARK: - Version Configuration
 
 let marketingVersion = "1.2.6"
-let currentProjectVersion = "31"
+let currentProjectVersion = "32"
 let developmentTeam = "2SB5Z68H26"
 
 // MARK: - Shared Settings
